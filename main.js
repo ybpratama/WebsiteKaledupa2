2692f1dca3ed38e4fd1bd8644963f9b8884c7d68
Gio-Farrel <giovanni.farrel.p@gmail.com>
2020-07-20 23:05:07 +0700

    Revert "About-Section-V1"
    This reverts commit a0e9977270c5ec46152f86fb500854a201a43a43.

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
 $(document).ready(function(){
-    // Video Pop up
     $(document).ready(function(){
         $('.venobox').venobox(); 
     });
-
-    // About Description Animation
-    function aos_init() {
-        AOS.init({
-          duration: 1000,
-          once: true
-        });
-      }
-      $(window).on('load', function() {
-        aos_init();
-      });
 });
 
